da4a38009eade7e7905124f7e097ce3013db9b51
domokrch <domokrch@domokrch.idk.com>
2024-08-16 00:59:27 +0500

    Second ch

---
+++ chapter_2_vars_and_primitive_types/ex/proj/DinoProfileGen.java
@@ -0,0 +1,13 @@
+public class DinoProfileGen {
+  public static void main(String args[]) {
+    String name = "Allo";
+    int age = 144000000;
+    String species = "Allosaurus";
+    String diet = "Carnivore";
+    int weight_kg = 1700;
+    String fireEmoji = "\uD83D\uDD25";
+
+    System.out.println("Meet " + name + ", a " + age + "-year-old " + species +
+". As a " + diet + ", it has a robust weight of " + weight_kg + " kilograms " + fireEmoji + fireEmoji + fireEmoji + " (i would like to thank the author for using kg)");
+  }
+}
